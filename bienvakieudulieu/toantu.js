e5400b39c9d33ef551d3c3e87c211b442ce899e8
kuro1907 <72237314+kuro1907@users.noreply.github.com>
2020-10-07 11:11:41 +0700

    bai tap kieu du lieu

---
+++ bienvakieudulieu/toantu.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt ("Nhập chiều rộng : ");
+inputHeight = prompt ("Nhập chiều cao :");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width*height;
+document.write("Diện tích là :" + area);
