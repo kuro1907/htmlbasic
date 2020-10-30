9295ed20fb9e3837bb4d50cad41e1e952845ed5c
kuro1907 <72237314+kuro1907@users.noreply.github.com>
2020-10-30 10:59:31 +0700

    bai tap mang

--- vonglap/baitapvonglap2.js
+++ vonglap/baitapvonglap2.js
@@ -41,14 +41,50 @@ function giaiThua() {
 }
 
 function tamGiac() {
-  let i = 0;
-  let n = 0;
-  while (n < 5) {
-    while (i < 5) {
+  for (let i = 1; i <= 5; i++) {
+    for (let j = 0; j < i; j++) {
       document.getElementById("demo3").innerHTML += "*";
-      document.getElementById("demo3").innerHTML += "</br>";
-      i++;
     }
-    n++;
+    document.getElementById("demo3").innerHTML += "</br>";
+  }
+  for (let i = 5; i >= 0; i--) {
+    for (let j = 0; j < i; j++) {
+      document.getElementById("demo3").innerHTML += "*";
+    }
+    document.getElementById("demo3").innerHTML += "</br>";
+  }
+  for (let i = 1; i <= 5; i++) {
+    for (let k = 0; k <= 5 - i; k++) {
+      document.getElementById("demo3").innerHTML += "&nbsp;&nbsp";
+    }
+    for (let j = 0; j < i; j++) {
+      document.getElementById("demo3").innerHTML += "*";
+    }
+    document.getElementById("demo3").innerHTML += "</br>";
+  }
+
+  for (let i = 1; i <= 5; i++) {
+    for (j = 1; j <= i; j++) {
+      document.getElementById("demo3").innerHTML += "&nbsp;&nbsp";
+    }
+
+    for (k = 0; k <= 5 - i; k++) {
+      document.getElementById("demo3").innerHTML += "*";
+    }
+    document.getElementById("demo3").innerHTML += "</br>";
+  }
+}
+
+function chuNhat() {
+  let space = "&nbsp;&nbsp;";
+  for (i = 1; i <= 7; i++) {
+    document.getElementById("demo4").innerHTML += "</br>" + "*";
+    for (j = 1; j <= 21; j++) {
+      if (i > 1 && j < 21 && i < 7) {
+        document.getElementById("demo4").innerHTML += space;
+      } else {
+        document.getElementById("demo4").innerHTML += "*";
+      }
+    }
   }
 }
